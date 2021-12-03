429866fdaf00710dd1d1537490d8d397e010af67
JasterV <victor@talentbait.com>
2021-12-03 14:48:14 +0100

    refactors part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -14,15 +14,7 @@ fn main() -> Result<()> {
 }
 
 fn extract_report_rates(report: &[String]) -> [i32; 2] {
-    let sums: Vec<i32> = report.iter().fold(vec![], |sums, curr| {
-        curr.chars()
-            .enumerate()
-            .map(|(index, char)| {
-                let value = if char == '0' { -1 } else { 1 };
-                sums.get(index).map_or(value, |sum| sum + value)
-            })
-            .collect()
-    });
+    let sums = compute_digits_sums(report);
     let gamma_rate: String = sums
         .iter()
         .map(|&sum| if sum > 0 { "1" } else { "0" })
@@ -37,6 +29,18 @@ fn extract_report_rates(report: &[String]) -> [i32; 2] {
         binary_str_to_int(&gamma_rate),
         binary_str_to_int(&epsilon_rate),
     ]
+}
+
+fn compute_digits_sums(report: &[String]) -> Vec<i32> {
+    report.iter().fold(vec![], |sums, curr| {
+        curr.chars()
+            .enumerate()
+            .map(|(index, char)| {
+                let value = if char == '0' { -1 } else { 1 };
+                sums.get(index).map_or(value, |sum| sum + value)
+            })
+            .collect()
+    })
 }
 
 fn read_diagnostic_report(file_path: &str) -> Result<Vec<String>> {
